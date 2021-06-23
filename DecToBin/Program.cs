aa82f5d303334e6bea4caf14f5432ba3d2470ea4
Veliafar <denis.seo@nxt.ru>
2021-06-23 20:53:53 +0300

    Добавьте файлы проекта.

---
+++ DecToBin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DecToBin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Киселев Денис
+            // 1. Реализовать функцию перевода из десятичной системы в двоичную, используя рекурсию.
+
+
+            Console.WriteLine("Здравствуйте! Эта программа переведет ваше целое число из десятичной системы в двличную.");
+
+            Console.Write("\nВведите число: ");
+            int decNum = Convert.ToInt32(Console.ReadLine());
+
+            string binary = ToBinary(decNum);
+
+            Console.WriteLine($"\nВаше число в двоичной форме: {binary}"); ;
+        }
+
+        static public string ToBinary(int n)
+        {
+            if (n < 2) return n.ToString();
+
+            var divisor = n / 2;
+            var remainder = n % 2;
+
+            return ToBinary(divisor) + remainder;
+        }
+    }
+}
---
+++ PowMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PowMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Киселев Денис
+            // 2. Реализовать функцию возведения числа a в степень b:
+
+
+            Console.WriteLine("Здравствуйте! Эта программа переведет ваше целое число из десятичной системы в двличную.");
+
+            Console.Write("\nВведите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nВведите степень: ");
+            int pow = Convert.ToInt32(Console.ReadLine());
+
+            num = ToPow(num, pow);
+            Console.WriteLine($"\nВаше число: {num}");
+        }
+
+        static int ToPow(int num, int pow)
+        {
+            int baseNum = num;
+            for (int i = 2; i <= pow; i++)
+            {
+                num *= baseNum;
+            }
+            return num;
+        }
+    }
+}
